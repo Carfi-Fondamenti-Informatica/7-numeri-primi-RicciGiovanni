de7018bb948f2ff6f2c7e565af90cc1649636486
RicciGiovanni <91595816+RicciGiovanni@users.noreply.github.com>
2021-12-01 10:14:40 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,17 +1,31 @@
 #include "lib.h"
- bool primo(int d, int n)
-{
-   
 
-    
-  if(n%d==0)
-  {
-      cout<<(n==d)<<endl;
-      
-      return (n==d);
-      
-  }
-    cout<<(n!=d)<<endl;
-    return primo(d+1,n);
-   
+int primo()
+{
+     int a,d=2,p=1;
+    cout<<"inserisci un numero intero"<<endl;
+    cin>>a;
+    while(d<a)
+    {
+        if(a%d==0)
+        {
+            p=0;
+            d=d+1;
+        }
+        else
+        {
+            d=d+1;
+        }
+       
+    }
+        
+    if(p==1)
+        {
+         cout<<"vero"<<endl;   
+        }
+        else
+        {
+           cout<<"falso"<<endl; 
+        }
+        return p;
 }
