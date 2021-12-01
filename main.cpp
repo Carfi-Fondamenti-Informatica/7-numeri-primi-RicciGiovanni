5e6a2dfd4e84f87a7c64bed31b4b61ba96f9b3cf
RicciGiovanni <91595816+RicciGiovanni@users.noreply.github.com>
2021-12-01 11:47:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
-  #include <iostream>
+#include <iostream>
 using namespace std;
-//#include "lib.h"
+#include "lib.h"
+bool primo(int,int);
+ int main(){
+   int numero=0;
+   bool a;
+   cin>>numero;
+  
+   a=primo(2,numero);
+  
+    if(a==0)
+    {
+        cout<<"numero non primo"<<endl;
+    }
+    else
+    {
+        cout<<"numero primo"<<endl;
+    }
+    return 0;
+   
+ }
  bool primo(int d, int n)
 {
   if(n%d==0)
@@ -21,21 +40,3 @@ using namespace std;
   }
     return primo(d+1,n);
 }
- int main(){
-   int numero=0;
-   bool a;
-   cin>>numero;
-  
-   a=primo(2,numero);
-  
-    if(a==0)
-    {
-        cout<<"numero non primo"<<endl;
-    }
-    else
-    {
-        cout<<"numero primo"<<endl;
-    }
-    return 0;
-   
- }
