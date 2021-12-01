07c31bd5ba6742508d7ad69327a32b8ec8ce62ba
RicciGiovanni <91595816+RicciGiovanni@users.noreply.github.com>
2021-12-01 10:26:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
-#include "lib.h"
+
 using namespace std;
+int primo()
+{
+     int a,d=2,p=1;
+    cout<<"inserisci un numero intero"<<endl;
+    cin>>a;
+    while(d<a)
+    {
+        if(a%d==0)
+        {
+            p=0;
+            d=d+1;
+        }
+        else
+        {
+            d=d+1;
+        }
+       
+    }
+        
+    if(p==1)
+        {
+         cout<<"vero"<<endl;   
+        }
+        else
+        {
+           cout<<"falso"<<endl; 
+        }
+        return p;
+}
 int main()
 {  
     int a;
