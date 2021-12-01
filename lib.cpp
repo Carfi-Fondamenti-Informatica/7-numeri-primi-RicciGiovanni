9d370c76e4cfb7d254133370f3de289954c35e48
RicciGiovanni <91595816+RicciGiovanni@users.noreply.github.com>
2021-12-01 09:46:50 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include "lib.h"
+ bool primo(int d, int n)
+{
+   
 
+    
+  if(n%d==0)
+  {
+      cout<<(n==d)<<endl;
+      
+      return (n==d);
+      
+  }
+    cout<<(n!=d)<<endl;
+    return primo(d+1,n);
+   
+}
